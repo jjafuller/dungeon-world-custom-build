8751bf4db7bfc8ef8134ac8169c982c7a2358c33
Jeffrey Fuller <jjafuller@gmail.com>
2013-07-04 15:13:06 -0400

    first four chapters are in good shape

--- static/build.py
+++ static/build.py
@@ -1,12 +1,61 @@
+from multiprocessing import Process
 from staticjinja import Renderer
+import easywatch
+import os
+import shutil
 
+def reset_assets():
+    print 'Resetting assets...'
+
+    root_path = os.path.dirname(os.path.realpath(__file__))
+
+    html_css = os.path.join(root_path, 'html/css/')
+    html_img = os.path.join(root_path, 'html/img/')
+    html_js = os.path.join(root_path, 'html/js/')
+
+    tmpl_css = os.path.join(root_path, 'template_assets/css/')
+    tmpl_img = os.path.join(root_path, 'template_assets/img/')
+    tmpl_js = os.path.join(root_path, 'template_assets/js/')
+
+    if os.path.exists(html_css):
+        shutil.rmtree(html_css)
+
+    if os.path.exists(html_img):
+        shutil.rmtree(html_img)
+
+    if os.path.exists(html_js):
+        shutil.rmtree(html_js)
+
+    shutil.copytree(tmpl_css, html_css)
+    shutil.copytree(tmpl_img, html_img)
+    shutil.copytree(tmpl_js, html_js)
+
+
+def asset_handler(event_type, src_path):
+    print src_path[:-4]
+    if(src_path[:-4] != ".tmp"):
+        dst_path = src_path.replace('/template_assets/', '/html/')
+        print 'Updating ' + dst_path
+        shutil.copy(src_path, dst_path)
+
+
+def watch_assets():
+    print 'Monitoring assets...'
+    easywatch.watch("./template_assets/", asset_handler)
+
+
+def watch_templates():
+    print 'Monitoring templates...'
+    renderer = Renderer(outpath="html")
+
+    renderer.run(debug=True, use_reloader=True)
 
 if __name__ == "__main__":
-    renderer = Renderer(
-        outpath="html"
-    )
+    reset_assets()
 
-    renderer.run(
-        debug=True, 
-        use_reloader=True
-    )
+    asset_monitor = Process(target=watch_assets)
+    asset_monitor.start()
+    
+    tmpl_monitor = Process(target=watch_templates)
+    tmpl_monitor.start()    
+    
